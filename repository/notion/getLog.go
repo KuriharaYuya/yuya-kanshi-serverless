5150d4e68a8367065d30f9f3b1605b955f84235d
kurihara_yuya <nikoand01@gmail.com>
2023-10-19 10:22:00 +0900

    fix

--- repository/notion/getLog.go
+++ repository/notion/getLog.go
@@ -9,20 +9,55 @@ import (
 	"github.com/jomei/notionapi"
 )
 
-type Database *notionapi.Database
+var client *notionapi.Client
 
-func GetLog() (Database, error) {
+func init() {
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Printf("読み込み出来ませんでした: %v", err)
 
 	}
-	integration_token := os.Getenv("NOTION_API_KEY")
+	client = CreateClient()
+}
 
-	client := notionapi.NewClient(notionapi.Token(integration_token))
-	db, err := client.Database.Get(context.Background(), notionapi.DatabaseID("8af74dfac9a0482bab353741bb355971"))
+func CreateClient() *notionapi.Client {
+	integration_token := os.Getenv("NOTION_API_KEY")
+	return notionapi.NewClient(notionapi.Token(integration_token))
+}
+
+func GetLog(date string) (interface{}, error) {
+	db, err := setDB()
 	if err != nil {
 		return nil, err
 	}
-	return db, nil
+
+	// クエリを作成
+	query := &notionapi.DatabaseQueryRequest{
+		Filter: notionapi.PropertyFilter{
+			Property: "allowPublish",
+			Checkbox: &notionapi.CheckboxFilterCondition{
+				Equals: true,
+			},
+		},
+	}
+
+	// クエリを使用してデータを取得
+	results, err := client.Database.Query(context.Background(), notionapi.DatabaseID(db.ID), query)
+	if err != nil {
+		fmt.Println("エラー")
+		return nil, err
+	}
+	tgt := results.Results[0].Properties
+	fmt.Println(tgt, "りざると")
+	return results, nil
+}
+
+// setDB関数はそのまま
+
+func setDB() (db *notionapi.Database, err error) {
+	db, err = client.Database.Get(context.Background(), notionapi.DatabaseID("8af74dfac9a0482bab353741bb355971"))
+	if err != nil {
+		return nil, err
+	}
+	return db, err
 }
