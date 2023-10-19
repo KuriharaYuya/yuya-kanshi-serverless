a0b93683b8aeb6f7970f3da660e2b39ad906bdc6
kurihara_yuya <nikoand01@gmail.com>
2023-10-19 16:31:50 +0900

    wip

--- repository/line/gateway.go
+++ repository/line/gateway.go
@@ -7,16 +7,23 @@ import (
 )
 
 func LineGateway(req utils.Request) {
-
-	var webhookData WebhookBody
-	err := json.Unmarshal([]byte(req.Body), &webhookData)
+	msg, err := extractMsg(req)
 	if err != nil {
-		// handle the error
+		return
+	}
+	ReplyToUser(msg)
+}
+
+func extractMsg(req utils.Request) (msg string, err error) {
+	var webhookData WebhookBody
+	err = json.Unmarshal([]byte(req.Body), &webhookData)
+	if err != nil {
+		return "", err
 	}
 
 	if len(webhookData.Events) > 0 && webhookData.Events[0].Message.Type == "text" {
 		textMessage := webhookData.Events[0].Message.Text
-		ReplyToUser(textMessage)
+		return textMessage, nil
 	}
-
+	return "", nil
 }
